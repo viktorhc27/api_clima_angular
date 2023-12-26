bad323c82947e3c46c2fd046fad9720e2bf583fe
viktorhc27 <victor.huanca1999@gmail.com>
2023-12-26 20:18:13 -0300

    http

--- src/app/service/clima.service.ts
+++ src/app/service/clima.service.ts
@@ -5,27 +5,16 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ClimaService {
-  private apiUrl = 'https://nominatim.openstreetmap.org/search';
+  
+  private apiUrl = 'https://api.weatherapi.com/v1/current.json'; // Update the API URL to use HTTPS
+  private apiKey = 'ef3dbfaf5994498d9ac230453232512';
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
+
   buscar_clima(ciudad: string): Observable<any> {
-    return this.http.get("http://api.weatherapi.com/v1/current.json?key=ef3dbfaf5994498d9ac230453232512&q="+ciudad+"&aqi=no&lang=es")
-    //return this.http.get("http://api.weatherapi.com/v1/current.json?key=ef3dbfaf5994498d9ac230453232512&q=arica&aqi=no")
-    
+    const url = `${this.apiUrl}?key=${this.apiKey}&q=${ciudad}&aqi=no&lang=es`;
+    return this.http.get(url);
   }
 
-  getLocation() {
-    return this.http.get('http://api.ipapi.com/api/check?access_key=AIzaSyCenp6Eupizf2ow5uX1NgMkZhMz-LtwOQQ')
-  }
-  getCities(query: string): Observable<any> {
-    const params = {
-      format: 'json',
-      limit: '5',
-      q: query
-    };
 
-    return this.http.get(this.apiUrl, { params });
-  }
 }
